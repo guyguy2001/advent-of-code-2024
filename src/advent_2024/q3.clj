92eda2dc639a6e9de5117c8eb85a847e5aaaaea9
Guy Geva <guygeva2311@gmail.com>
2024-12-03 23:59:40 +0200

    Day 3

---
+++ src/advent_2024/q3.clj
@@ -0,0 +1,52 @@
+(ns advent-2024.q3
+  (:require
+   [clojure.string :as str]))
+
+(def test-path "resources\\q3\\test.txt")
+(def input-path "resources/q3/input.txt")
+(def test2-path "resources\\q3\\test2.txt")
+(def input2-path "resources/q3/input2.txt")
+
+(defn read-input
+  [text]
+  (map (fn [[_all op b c :as f]] (cond
+                                   (str/starts-with? _all "mul") [:mul (Integer. b) (Integer. c)]
+                                   (= _all "do()") [:do]
+                                   (= _all "don't()") [:dont]
+                                   :else [f]))
+       (re-seq #"(mul)\((\d+),(\d+)\)|(don't)\(\)|(do)\(\)" text)))
+
+(defn answer-a
+  [text]
+  (reduce (fn [sum [_ a b]] (+ sum (* a b)))
+          0
+          (read-input text)))
+
+(defn answer-b
+  [text]
+  (first
+   (reduce (fn [[sum active] [op a b]]
+             (cond
+               (= op :mul) (if active
+                             [(+ sum (* a b)) active]
+                             [sum active])
+               (= op :do) [sum true]
+               (= op :dont) [sum false]
+               :else [sum active]))
+           [0 true]
+           (read-input text))))
+
+
+
+(comment
+  (def input (slurp test-path))
+  input
+  (re-seq #"mul" input)
+  (read-input input)
+  (read-input (slurp test2-path))
+  foo
+
+  (read-input (slurp input-path))
+  (answer-a input)
+  (answer-b (slurp input-path))
+  :rcf)
