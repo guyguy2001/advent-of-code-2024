f83c6f5d7cf7db5964fd1bfe36a8f0fe215c9734
guyguy2001 <guygeva2311@gmail.com>
2024-12-05 20:38:17 +0200

    4.a complex solution

--- src/advent_2024/q4.clj
+++ src/advent_2024/q4.clj
@@ -4,6 +4,14 @@
 (def test-path "resources/q4/test.txt")
 (def input-path "resources/q4/input.txt")
 
+(defn cols
+  [grid]
+  (count (get grid 0)))
+
+(defn rows
+  [grid]
+  (count grid))
+
 (defn col
   [grid x]
   (map
@@ -24,6 +32,57 @@
   [grid y]
   (reverse (row grid y)))
 
+(defn slash-diags
+  [grid]
+  (for [start (concat
+               (map (fn [y] [0 y]) (range (dec (rows grid))))
+               (map (fn [x] [x (dec (rows grid))]) (range (cols grid))))]
+    (loop [x (first start)
+           y (second start)
+           result []]
+      (if (and (<= 0 x (dec (cols grid)))
+               (<= 0 y (dec (rows grid))))
+        (recur (inc x)
+               (dec y)
+               (into result [(get-in grid [y x])]))
+        result))))
+
+(defn slash-rev-diags
+  [grid]
+  (map reverse (slash-diags grid)))
+
+(defn backslash-diags
+  [grid]
+  (for [start (concat
+                              ; todo order
+               (map (fn [y] [0 (- (rows grid) y 1)]) (range (dec (rows grid))))
+               (map (fn [x] [x 0]) (range (cols grid))))]
+    (loop [x (first start)
+           y (second start)
+           result []]
+      (if (and (<= 0 x (dec (cols grid)))
+               (<= 0 y (dec (rows grid))))
+        (recur (inc x)
+               (inc y)
+               (into result [(get-in grid [y x])]))
+        result))))
+
+(defn backslash-rev-diags
+  [grid]
+  (map reverse (backslash-diags grid)))
+
+(comment
+  (def grid ["ABC"
+             "DEF"
+             "GHI"])
+
+  (slash-diags grid)
+  (slash-rev-diags grid)
+  (backslash-diags grid)
+  (backslash-rev-diags grid)
+
+  :rcf)
+
 (defn find-marching
   [coll pattern]
   (let [index-to-progress (reduce (fn [progress [i char]]
@@ -41,6 +100,44 @@
                                                         (empty? v)))) index-to-progress)]
     (map (fn [[k v]] k) filtered)))
 
+(defn occurrences-in-grid
+  [grid pattern]
+  [(map (fn [r] (map (fn [c] [c r])
+                     (find-marching (row grid r) pattern)))
+        (range (rows grid)))
+   (map (fn [r] (map (fn [c] [(- (count (get grid r)) c 1) r])
+                     (find-marching (row-rev grid r) pattern)))
+        (range (rows grid)))
+   (map (fn [c] (map (fn [r] [c r])
+                     (find-marching (col grid c) pattern)))
+        (range (cols grid)))
+   (map (fn [c] (map (fn [r] [c (- (count grid) r 1)])
+                     (find-marching (col-rev grid c) pattern)))
+        (range (cols grid)))
+   (map #(find-marching % pattern)
+        (slash-diags grid))
+   (map #(find-marching % pattern)
+        (slash-rev-diags grid))
+   (map #(find-marching % pattern)
+        (backslash-diags grid))
+   (map #(find-marching % pattern)
+        (backslash-rev-diags grid))])
+
+(defn read-input
+  [text]
+  (str/split-lines text))
+
+(comment
+  (str/split-lines "XMASXMAS\r\nXMASXMAS")
+  :rcf)
+
+(defn solve-q4-1
+  [text]
+  (-> text
+      (read-input)
+      (occurrences-in-grid "XMAS")
+      (count)))
+
 (comment
   (def grid ["XMAS" "MASX" "ASXM" "SXMA" "XMAS" "MASX" "ASXM" "SXMA"])
   (def pattern "XMAS")
@@ -53,16 +150,16 @@
   (count (apply concat (apply concat
                               (map (fn [r] (map (fn [c] [c r])
                                                 (find-marching (row grid r) pattern)))
-                                   (range 4))
+                                   (range (rows grid)))
                               (map (fn [r] (map (fn [c] [(- (count (get grid r)) c 1) r])
                                                 (find-marching (row-rev grid r) pattern)))
-                                   (range 4))
+                                   (range (rows grid)))
                               (map (fn [c] (map (fn [r] [c r])
                                                 (find-marching (col grid c) pattern)))
-                                   (range 4))
+                                   (range (cols grid)))
                               (map (fn [c] (map (fn [r] [c (- (count grid) r 1)])
                                                 (find-marching (col-rev grid c) pattern)))
-                                   (range 4)))))
+                                   (range (cols grid))))))
   :rcf)
 
 (comment
