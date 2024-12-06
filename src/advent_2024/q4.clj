2048f8030e43e67c391c42394fe294e3450cf28b
guyguy2001 <guygeva2311@gmail.com>
2024-12-06 15:15:40 +0200

    Day 4 solution

--- src/advent_2024/q4.clj
+++ src/advent_2024/q4.clj
@@ -12,116 +12,6 @@
   [grid]
   (count grid))
 
-(defn col
-  [grid x]
-  (map
-   #(get-in grid [% x])
-   (range (count grid))))
-
-(defn col-rev
-  [grid x]
-  (map
-   #(get-in grid [(- (count grid) % 1) x])
-   (range (count grid))))
-
-(defn row
-  [grid y]
-  (get grid y))
-
-(defn row-rev
-  [grid y]
-  (reverse (row grid y)))
-
-(defn slash-diags
-  [grid]
-  (for [start (concat
-               (map (fn [y] [0 y]) (range (dec (rows grid))))
-               (map (fn [x] [x (dec (rows grid))]) (range (cols grid))))]
-    (loop [x (first start)
-           y (second start)
-           result []]
-      (if (and (<= 0 x (dec (cols grid)))
-               (<= 0 y (dec (rows grid))))
-        (recur (inc x)
-               (dec y)
-               (into result [(get-in grid [y x])]))
-        result))))
-
-(defn slash-rev-diags
-  [grid]
-  (map reverse (slash-diags grid)))
-
-(defn backslash-diags
-  [grid]
-  (for [start (concat
-                              ; todo order
-               (map (fn [y] [0 (- (rows grid) y 1)]) (range (dec (rows grid))))
-               (map (fn [x] [x 0]) (range (cols grid))))]
-    (loop [x (first start)
-           y (second start)
-           result []]
-      (if (and (<= 0 x (dec (cols grid)))
-               (<= 0 y (dec (rows grid))))
-        (recur (inc x)
-               (inc y)
-               (into result [(get-in grid [y x])]))
-        result))))
-
-(defn backslash-rev-diags
-  [grid]
-  (map reverse (backslash-diags grid)))
-
-(comment
-  (def grid ["ABC"
-             "DEF"
-             "GHI"])
-
-  (slash-diags grid)
-  (slash-rev-diags grid)
-  (backslash-diags grid)
-  (backslash-rev-diags grid)
-
-  :rcf)
-
-(defn find-marching
-  [coll pattern]
-  (let [index-to-progress (reduce (fn [progress [i char]]
-                                    (let [progress (into progress {i pattern})
-                                          progress (into {} (map (fn [[k v]] [k (cond
-                                                                                  (boolean? v) v
-                                                                                  (empty? v) true
-                                                                                  (= (first v) char) (rest v)
-                                                                                  :else false)])
-                                                                 progress))]
-                                      progress))
-                                  {}
-                                  (map-indexed (fn [i v] [i v]) coll))
-        filtered (filter (fn [[k v]] (or (true? v) (and (not (false? v))
-                                                        (empty? v)))) index-to-progress)]
-    (map (fn [[k v]] k) filtered)))
-
-(defn occurrences-in-grid
-  [grid pattern]
-  [(map (fn [r] (map (fn [c] [c r])
-                     (find-marching (row grid r) pattern)))
-        (range (rows grid)))
-   (map (fn [r] (map (fn [c] [(- (count (get grid r)) c 1) r])
-                     (find-marching (row-rev grid r) pattern)))
-        (range (rows grid)))
-   (map (fn [c] (map (fn [r] [c r])
-                     (find-marching (col grid c) pattern)))
-        (range (cols grid)))
-   (map (fn [c] (map (fn [r] [c (- (count grid) r 1)])
-                     (find-marching (col-rev grid c) pattern)))
-        (range (cols grid)))
-   (map #(find-marching % pattern)
-        (slash-diags grid))
-   (map #(find-marching % pattern)
-        (slash-rev-diags grid))
-   (map #(find-marching % pattern)
-        (backslash-diags grid))
-   (map #(find-marching % pattern)
-        (backslash-rev-diags grid))])
 
 (defn read-input
   [text]
@@ -131,71 +21,152 @@
   (str/split-lines "XMASXMAS\r\nXMASXMAS")
   :rcf)
 
+(defn march-through-grid
+  "For example: (get-marching ([0 0] [1 0] ... [4 0]) [0 1] 5 5) => [[0 0] [0 1] ... [0 4]] [[1 0] [1 1]]"
+  [[dx dy] [x y] width height]
+  (loop [x x
+         y y
+         result []]
+    (if (and (<= 0 x (dec width))
+             (<= 0 y (dec height)))
+      (recur (+ x dx)
+             (+ y dy)
+             (into result [[x y]]))
+      result)))
+
+(defn row-marches
+  [width height]
+  (map #(march-through-grid [1 0] [0 %] width height)
+       (range height)))
+
+(defn col-marches
+  [width height]
+  (map #(march-through-grid [0 1] [% 0] width height)
+       (range width)))
+
+(defn slash-diag-marches
+  [width height]
+  (for [[x y] (concat
+               (map (fn [y] [0 y]) (range (dec height)))
+               (map (fn [x] [x (dec height)]) (range width)))]
+    (march-through-grid [1 -1] [x y] width height)))
+
+(defn backslash-diag-marches
+  [width height]
+  (for [[x y] (concat
+               (map (fn [y] [0 (- height y 1)]) (range (dec height)))
+               (map (fn [x] [x 0]) (range width)))]
+    (march-through-grid [1 1] [x y] width height)))
+
+(defn find-marching-in-grid
+  [grid march pattern]
+  (let [found-starting-points (find-marching (map (fn [[x y]] (get-in grid [y x])) march)
+                                             pattern)]
+    (map #(nth march %) found-starting-points)))
+
+(defn multi-find-marching-in-grid-with-symbol
+  [grid marches pattern symbol]
+  (apply concat (map (fn [march] (map (fn [v] [symbol v])
+                                      (find-marching-in-grid grid march pattern)))
+                     marches)))
+
+(comment
+  (march-through-grid [1 1] [2 0] 5 5)
+  (slash-diag-marches 5 5)
+  (backslash-diag-marches 5 5)
+
+  (let [march (get (vec (slash-diag-marches 5 5)) 4)
+        found-starting-points (find-marching (map (fn [[x y]] (get-in grid [y x])) march)
+                                             "XM")]
+    (map #(get march %) found-starting-points))
+  (find-marching-in-grid grid (get (vec (slash-diag-marches 5 5)) 4) "XM")
+  (multi-find-marching-in-grid-with-symbol grid (get (vec (slash-diag-marches 5 5)) 4) "XM" "/")
+  :rcf)
+
+(defn occurrences-in-grid2
+  [grid pattern]
+  (apply concat [(multi-find-marching-in-grid-with-symbol grid (row-marches (cols grid) (rows grid)) pattern "-")
+                 (multi-find-marching-in-grid-with-symbol grid (map reverse (row-marches (cols grid) (rows grid))) pattern "!-")
+                 (multi-find-marching-in-grid-with-symbol grid (col-marches (cols grid) (rows grid)) pattern "|")
+                 (multi-find-marching-in-grid-with-symbol grid (map reverse (col-marches (cols grid) (rows grid))) pattern "!|")
+                 (multi-find-marching-in-grid-with-symbol grid (slash-diag-marches (cols grid) (rows grid)) pattern "/")
+                 (multi-find-marching-in-grid-with-symbol grid (map reverse (slash-diag-marches (cols grid) (rows grid))) pattern "!/")
+                 (multi-find-marching-in-grid-with-symbol grid (backslash-diag-marches (cols grid) (rows grid)) pattern "\\")
+                 (multi-find-marching-in-grid-with-symbol grid (map reverse (backslash-diag-marches (cols grid) (rows grid))) pattern "!\\")]))
+
+; actual solution
 (defn solve-q4-1
   [text]
   (-> text
       (read-input)
-      (occurrences-in-grid "XMAS")
+      (occurrences-in-grid2 "XMAS")
       (count)))
 
+(def target-cross (seq "SMMSA"))
+
+(defn make-base-cross
+  "If the input is [1 0], returns right up left down center"
+  [[axis-x axis-y]]
+  [[axis-x axis-y] [(- axis-y) axis-x] [(- axis-x) (- axis-y)]
+   [axis-y (- axis-x)] [0 0]])
+; TODO: symetrically reversed cross
+(defn make-cross
+  [[x y] [axis-x axis-y]]
+  (map (fn [[a b]] [(+ x a) (+ y b)])
+       (make-base-cross [axis-x axis-y])))
+(defn find-crosses
+  [grid pattern]
+  (count (filter identity (for [x (range (cols grid))
+                                y (range (rows grid))
+                                orientation [;[0 1] [-1 0] [0 -1] [1 0]
+                                             [1 1] [1 -1] [-1 1] [-1 -1]]]
+                            (= (map (fn [[x y]] (get-in grid [y x]))
+                                    (make-cross [x y] orientation))
+                               pattern)))))
+(defn solve-q4-2
+  [text]
+  (-> text
+      (read-input)
+      (find-crosses target-cross)))
+
 (comment
-  (def grid ["XMAS" "MASX" "ASXM" "SXMA" "XMAS" "MASX" "ASXM" "SXMA"])
-  (def pattern "XMAS")
-  (map (fn [r] [0 r])
-       (find-marching (col grid 0) pattern))
-  (apply concat (map (fn [c] (map (fn [r] [c r])
-                                  (find-marching (col grid c) pattern)))
-                     (range 4)))
-  ; TODO: I think this solves it?
-  (count (apply concat (apply concat
-                              (map (fn [r] (map (fn [c] [c r])
-                                                (find-marching (row grid r) pattern)))
-                                   (range (rows grid)))
-                              (map (fn [r] (map (fn [c] [(- (count (get grid r)) c 1) r])
-                                                (find-marching (row-rev grid r) pattern)))
-                                   (range (rows grid)))
-                              (map (fn [c] (map (fn [r] [c r])
-                                                (find-marching (col grid c) pattern)))
-                                   (range (cols grid)))
-                              (map (fn [c] (map (fn [r] [c (- (count grid) r 1)])
-                                                (find-marching (col-rev grid c) pattern)))
-                                   (range (cols grid))))))
+  ;level 2
+  (make-cross [1 1] [0 1])
+  (make-cross [1 1] [1 1])
+  (def grid (read-input (slurp test-path)))
+  (map (fn [[x y]] (get-in grid [y x]))
+       (make-cross [1 1] [1 1]))
+  (count (filter identity (for [x (range (cols grid))
+                                y (range (rows grid))
+                                orientation [[0 1] [-1 0] [0 -1] [1 0]
+                                             [1 1] [1 -1] [-1 1] [-1 -1]]]
+                            (= (map (fn [[x y]] (get-in grid [y x]))
+                                    (make-cross [x y] orientation))
+                               target-cross)))) ; p sure I'm missing 8 options
   :rcf)
-
 (comment
-  (def grid [["a" "b"] ["c" "d"]])
-  (col grid 1)
-  (col-rev grid 1)
-  (row grid 1)
-  (row-rev grid 1)
-  (def coll "XMASXXMAS")
-  (map vector (range (count coll)) coll)
-  (let [i 1
-        c \M
-        progress `{0 (\M \A \S)}
-        progress (into progress {i "XMAS"})
-        progress
-        (into {} (map (fn [[k v]] [k (cond
-                                       (empty? v) true
-                                       (= (first v) c) (rest v)
-                                       :else false)])
-                      progress))]
-    progress)
 
-  (reduce (fn [progress [i char]]
-            (let [progress (into progress {i "XMAS"})
-                  progress (into {} (map (fn [[k v]] [k (cond
-                                                          (boolean? v) v
-                                                          (empty? v) true
-                                                          (= (first v) char) (rest v)
-                                                          :else false)])
-                                         progress))]
-              progress))
-          {}
-          (map-indexed (fn [i v] [i v]) coll))
-
-  (find-marching coll "XMAS")
-
+  (def grid (read-input (slurp test-path)))
+  grid
+  ["MMMSXXMASM"
+   "MSAMXMSMSA"
+   "AMXSXMAAMM"
+   "MSAMASMSMX"
+   "XMASAMXAMM"
+   "XXAMMXXAMA"
+   "SMSMSASXSS"
+   "SAXAMASAAA"
+   "MAMMMXMMMM"
+   "MXMXAXMASX"]
 
   (empty? `())
+
+  *e
+  (count (occurrences-in-grid2 (read-input (slurp test-path)) "XMAS"))
+
+  (solve-q4-1 (slurp test-path))
+  (solve-q4-1 (slurp input-path))
+  (solve-q4-2 (slurp test-path))
+  (solve-q4-2 (slurp input-path))
+  (read-input (slurp test-path))
   :rcf)
